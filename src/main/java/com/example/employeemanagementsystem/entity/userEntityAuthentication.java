cd6ece6a8ba58b3f2c71c8b1b97b920aac98c369
unknown <kanchanrshende19@gmail.com>
2025-06-17 04:47:49 -0700

    push all task on new branch

---
+++ src/main/java/com/example/employeemanagementsystem/entity/userEntityAuthentication.java
@@ -0,0 +1,18 @@
+package com.example.employeemanagementsystem.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Table(name="userEntity")
+@Entity
+public class userEntityAuthentication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String Username;
+    private String Password;
+    private String Role;
+}
---
+++ src/main/java/com/example/employeemanagementsystem/repository/userEntityRepository.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagementsystem.repository;
+
+import com.example.employeemanagementsystem.entity.userEntityAuthentication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface userEntityRepository extends JpaRepository<userEntityAuthentication,Long> {
+    Optional<userEntityAuthentication> findByUsername(String username);
+
+}
